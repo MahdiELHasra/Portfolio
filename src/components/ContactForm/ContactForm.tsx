6f989768e3d6cec2d01d92944474474d7e7c9425
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-04-09 11:46:14 +0000

    chore: Update ContactForm component styles and responsiveness

---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,99 @@
+import { useForm, ValidationError } from "@formspree/react";
+import { useContext, useEffect } from "react";
+import toast from "react-hot-toast";
+import { store } from "../../App";
+import playAudio from "../../utils/helpers/playAudio";
+import "./ContactForm.scss";
+
+export default function ContactForm() {
+  const [formState, handleSubmit] = useForm("xrgnjkjz");
+  const [state, dispatch] = useContext(store);
+  useEffect(() => {
+    if (formState.succeeded) {
+      toast.success(
+        state.language === "english"
+          ? "Nachricht abgeschickt!"
+          : "Message sent!",
+        {
+          position: "top-left",
+          style: {
+            border: state.darkmode
+              ? "2px solid hsla(0,0%,51.4%,.16)"
+              : "1px solid #ebebeb",
+            boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.225)",
+            borderRadius: "14px",
+            fontFamily: "Inter",
+            color: state.darkmode ? "white" : "#1d1d1f",
+            fontSize: "14px",
+            backgroundColor: state.darkmode ? "#141414" : "",
+          },
+        }
+      );
+    }
+  }, [formState.succeeded, state.language]);
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`contact-form-container ${
+        state.darkmode ? "dark-card" : "light-card"
+      } ${state.darkmode ? "dark-shadow" : "light-shadow"}`}
+    >
+      <div className="input-container">
+        <input
+          className={`field data ${
+            state.darkmode ? "dark-field" : "light-field"
+          }`}
+          type="text"
+          name="user_name"
+          placeholder={state.language === "english" ? "Ihr Name" : "Your name"}
+          required
+        />
+        <ValidationError
+          prefix="Email"
+          field="user_name"
+          errors={state.errors}
+        />
+        <input
+          className={`field data ${
+            state.darkmode ? "dark-field" : "light-field"
+          }`}
+          type="email"
+          name="user_email"
+          placeholder={
+            state.language === "english"
+              ? "Ihre E-Mail-Adresse"
+              : "Your email address"
+          }
+          required
+        />
+        <ValidationError
+          prefix="Email"
+          field="user_email"
+          errors={state.errors}
+        />
+      </div>
+      <textarea
+        name="message"
+        placeholder={
+          state.language === "english"
+            ? "Ihre Nachricht an mich"
+            : "Your message for me"
+        }
+        className={`field ${state.darkmode ? "dark-field" : "light-field"}`}
+        required
+      />
+      <ValidationError prefix="Email" field="message" errors={state.errors} />
+      <button
+        type="submit"
+        className="submit"
+        value={state.language === "english" ? "Absenden" : "Submit"}
+        disabled={state.submitting}
+        onMouseDown={playAudio}
+        onMouseUp={playAudio}
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
